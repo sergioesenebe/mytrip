3c3969dc068eece86bcb2f42d05d98b0529d2d87
sergioesenebe <sergio.nunez@esenebe.com>
2025-01-22 12:56:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ $db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_password = getenv('DB_PASSWORD');
 $db_name = getenv('DB_NAME');
-echo $db_host;
+$db_port = getenv('DB_PORT');
 // Mostrar mensaje de inicio
 echo "<h1>¡Tu página PHP está funcionando!</h1>";
 
 // Intentar conexión a la base de datos
 echo "<h2>Verificando conexión a la base de datos...</h2>";
 
-$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
 
 if ($conn->connect_error) {
     echo "<p style='color: red;'>Error de conexión: " . $conn->connect_error . "</p>";
